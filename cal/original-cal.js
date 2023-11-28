55e121c1efb9817904ea44690d698c53d2351c44
Workspacecoding <149985645+Workspacecoding@users.noreply.github.com>
2023-11-28 16:42:03 +0800

    Original-cal-profile
    JS unencrypted

---
+++ cal/original-cal.js
@@ -0,0 +1,32 @@
+
+function calculateAmortization() {
+    // 取得使用者輸入的值
+    const loanAmount = parseFloat(document.getElementById('loanAmount').value);
+    const interestRate = parseFloat(document.getElementById('interestRate').value);
+    const loanTerm = parseInt(document.getElementById('loanTerm').value);
+
+    // 計算每期的本金攤還額
+    const monthlyInterestRate = interestRate / 100 / 12;
+    const monthlyPayment =  (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -loanTerm));
+
+    // 初始化表格
+    const tableBody = document.getElementById('amortizationTableBody');
+    tableBody.innerHTML = '';
+
+    let remainingBalance = loanAmount;
+
+    // 計算每期的攤還情況
+    for (let period = 1; period <= loanTerm; period++) {
+        const interestPayment = remainingBalance * monthlyInterestRate;
+        const principalPayment = monthlyPayment - interestPayment;
+        remainingBalance -= principalPayment;
+
+        // 創建表格行並插入表格
+        const row = tableBody.insertRow();
+        row.insertCell(0).innerText = period;
+        row.insertCell(1).innerText = Math.ceil(principalPayment.toFixed(2));
+        row.insertCell(2).innerText = Math.ceil(interestPayment.toFixed(2));
+        row.insertCell(3).innerText = Math.ceil(monthlyPayment.toFixed(2));
+        row.insertCell(4).innerText = Math.ceil(remainingBalance.toFixed(2));
+    }
+}
