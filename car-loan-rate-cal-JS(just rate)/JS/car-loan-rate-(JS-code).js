456d5ffbf3b66c9746955da4bf562fc06ea2561b
Workspacecoding <149985645+Workspacecoding@users.noreply.github.com>
2023-12-14 10:35:35 +0800

    Add files via upload

---
+++ car-loan-rate-cal-JS(just rate)/JS/car-loan-rate-(JS-code).js
@@ -0,0 +1,13 @@
+document.getElementById('countID').addEventListener('click', function() {
+    var interestRate = parseInt(document.getElementById('rateID').value);
+    var moneyNum = parseInt(document.getElementById('moneyID').value);
+    var monthNum = parseInt(document.getElementById('monthID').value);
+
+    var monthlyInterestRate = interestRate / 1200;
+    var monthlyPayment = (moneyNum * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, monthNum)) / (Math.pow(1 + monthlyInterestRate, monthNum) - 1);
+    var totalInterest = monthlyPayment * monthNum - moneyNum;
+    var totalAmount = monthlyPayment * monthNum;
+
+    document.getElementById('monthlyPaymentID').textContent = Math.ceil(monthlyPayment);
+   
+});
